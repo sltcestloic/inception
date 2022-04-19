adc9c39315ad34b2fc16ab7aa0cc93857a632570
wshloic <lbertran@student.42lyon.fr>
2022-04-19 16:22:50 +0200

    fuckladb

--- Makefile
+++ Makefile
@@ -1,7 +1,21 @@
-stop:
-	docker stop nginx
-	docker stop wordpress
-	docker stop mariadb
+all:
+	mkdir -p volumes
+	docker-compose -f srcs/docker-compose.yml up -d
 
-rebuild: stop
-	docker-compose -f srcs/docker-compose.yml up -d --build 
+build:
+	docker-compose -f srcs/docker-compose.yml build
+
+up:
+	docker-compose -f srcs/docker-compose.yml up -d
+
+down:
+	docker-compose -f srcs/docker-compose.yml down
+
+fclean:
+	rm -rf volumes
+	docker image rm -f nginx wordpress mariadb
+	docker-compose -f srcs/docker-compose.yml down -v
+
+re: fclean all
+
+.PHONY: re fclean down up build all
--- volumes/wordpress/wp-config.php
+++ volumes/wordpress/wp-config.php
@@ -11,6 +11,7 @@
  * * Database settings
  * * Secret keys
  * * Database table prefix
+ * * Localized language
  * * ABSPATH
  *
  * @link https://wordpress.org/support/article/editing-wp-config-php/
@@ -20,7 +21,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'inceptiondb' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
 define( 'DB_USER', 'user' );
@@ -48,14 +49,16 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',          '@!-mgu^Lu%NK(op{tCbr+H;^ ;=xZZS@lys7k>M-> jvgiXR)L=&K#~5}TS&gjiz' );
+define( 'SECURE_AUTH_KEY',   'y39lCu7ir*.$K4#(5~G?[i40N$k4^`S-[qJX(ZewKcQ2/tlBIL[cBARbg4A>NStm' );
+define( 'LOGGED_IN_KEY',     'n4%Zs;djkg=3n8!f*1#j]yuZI;SJ.}g-S@%>q*&]X6PjK9z]fl~V mi$OL7vh1}l' );
+define( 'NONCE_KEY',         'x$b47 [sms~a$!QV9=JXQ)ZO.q*WX/rQjfA}>B7;xFUGqBv8)0s$!pMvSqkR6E%2' );
+define( 'AUTH_SALT',         'iaK O%ijtGn$p(D;Vko8Gf1aVe~o U@l[#*}nD}PX0[6~f>7QZN=~n(y!;q{-Q7O' );
+define( 'SECURE_AUTH_SALT',  '=8>j+&%a6bCExkz}~!DGj`G;;rRd*`3D5fV`U,R%2/LDD4V-[ ZjXATX{s94F$N6' );
+define( 'LOGGED_IN_SALT',    'eH_2*-E(]hxaI+D>ld-!%nhMO;5I}_jc,9!%q%{Ep*!3.qpRVye~Y<4>T~S<6%,Q' );
+define( 'NONCE_SALT',        '{WY|yUqDUR7gg&QuYWibJ?mc~C9ae(oFx91ZE{asVI7m}KU4E.C%K?uk0q@rYR>f' );
+define( 'WP_CACHE_KEY_SALT', '<@Gi1#jDK?<>Q|7uSe/]5(QNJ.@bnKX|vAir{y0CAzV^wb{)*qPwO5vE^L<qmeut' );
+
 
 /**#@-*/
 
@@ -80,6 +83,7 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
+
 
 /* Add any custom values between this line and the "stop editing" line. */
 
